c7ddbd35d6e5fe63b247c656badffe7e171115d6
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-22 14:06:02 +0200

    Styled the heading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,15 @@ import memories from "./images/memories.png"
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 
+import useStyles from "./styles"
+
 export default function App() {
+  const classes = useStyles()
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
