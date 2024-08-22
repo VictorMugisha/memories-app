95971aa404e5f4a98a3bdb52d3967238f63f6bd6
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-22 12:17:15 +0200

    Connected to the mongoDB database using connection string

--- server/index.js
+++ server/index.js
@@ -8,3 +8,11 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://victormugisha:victormugisha123@nodenetninja.hd6g2.mongodb.net/?retryWrites=true&w=majority&appName=NodeNetNinja";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
